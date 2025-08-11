4659e9078726552a919ded697f7afd3e19090806
sadxdev <sadxdev@gmail.com>
2025-08-11 20:12:18 +0530

    Initial commit

---
+++ src/main/java/com/bashverse/rapidorderservice/RapidOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bashverse.rapidorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RapidOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RapidOrderServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bashverse/rapidorderservice/RapidOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bashverse.rapidorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RapidOrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
